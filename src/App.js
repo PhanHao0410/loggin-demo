6f71230d163f525c1ad7c3142ae4ea34a61e4d3f
Phan Hào <phanthanhhao3010@gmail.com>
2023-03-07 20:02:34 +0700

    Login ui and validate

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
 import './App.scss';
-
+import Loggin from './asesst/view/loggin/Loggin';
+import ForgetPassword from './asesst/view/loggin/forgetPassword';
+import CreatNewPassword from './asesst/view/loggin/CreatNewPassword';
+// import {
+//   BrowserRouter,
+//   Switch,
+//   Route,
+//   Routes
+// } from "react-router-dom";
 function App() {
-  const [hide, setHide] = useState(true);
-  function getValue(id) {
-    return document.getElementById(id).value.trim();
-  }
-  function validate() {
-    let email = getValue("email");
-    let checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!checkEmail.test(email)) {
-      alert(`Email: ${email} không hợp lệ`);
-      return false;
-    }
-    let password = getValue("password");
-    let hashUpperFirst = password.match(/[A-Z]/);
-    let hashLower = password.match(/[a-z]/g);
-    let hashNumber = password.match(/[0-9]/g);
-    if (!password || !hashUpperFirst || !hashLower || !hashNumber || password.length < 8) {
-      alert(`Password: ${password} không hợp lệ`);
-      return false;
-    } else { }
-    alert(`Email: ${email} - Password: ${password} nhập đúng`);
-    return true;
-
-  }
   return (
+    // <BrowserRouter>
     <div className="App">
-      <div className="loggin">
-        <div className="form-singIn">
-          <p>Sign In</p>
-          <input type="email" placeholder="Enter your email" id="email" /><br />
-          <div className="passwordInput">
-            <input type={hide ? "password" : "texr"} placeholder="Enter your password" id="password" />
-            <span onClick={() => setHide(!hide)}><i class={hide ? "fa-regular fa-eye-slash" : "fa-regular fa-eye"}></i></span>
-          </div>
-
-          <button onClick={() => validate()}>SIGN IN</button><br />
-          <a>Forgot Password?</a>
-        </div>
-      </div>
-      <div className="loggin-right"></div>
+      <header>
+        <Loggin />
+      </header>
+      {/* <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        /> */}
     </div>
+    // </BrowserRouter>
   );
 }
 
---
+++ src/asesst/view/loggin/forgetPassword.js
@@ -0,0 +1,39 @@
+// import React, { useState } from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import "./forgetPassword.scss";
+// import { withRouter } from 'react-router-dom';
+
+
+// function ForgetPassword() {
+//     const [errorEmail, setErrorEmail] = useState(null);
+//     function handleChangeEmail(event) {
+//         let email = event.target.value.trim();
+//         let checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//         let emailIsValid = checkEmail.test(email);
+//         if (email === "") {
+//             setErrorEmail("Please input the email.");
+//             return false;
+//         };
+//         if (!emailIsValid) {
+//             setErrorEmail("The email is invalid.");
+//             return false
+//         };
+//         setErrorEmail(null);
+//         return true;
+//     }
+//     return (
+//         <div className="Loggin">
+//             <div className="loggin-forgot">
+//                 <div className="form-singIn">
+//                     <p>Forgot Password</p>
+//                     <input className={errorEmail ? "inputError" : ""} onChange={handleChangeEmail} type="email" placeholder="Enter your email" id="email" /><br />
+//                     {errorEmail && <div style={{ color: "red" }} className="showErrorEmail">{errorEmail}</div>}
+//                     <button onClick="handleChangeEmail();handleChangePasssword()" >SEND INSTRUCTION</button><br />
+//                     <div className="title-bottom-forget">Do not forget to check your SPAM box.</div>
+//                 </div>
+//             </div>
+//             <div className="loggin-right"></div>
+//         </div>
+//     )
+// }
+// export default withRouter(ForgetPassword);
